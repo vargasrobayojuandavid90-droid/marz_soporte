6e969b98113bc7c5c8a64019e218e042b79e413f
vargasrobayojuandavid90-droid <vargasrobayojuandavid90@gmail.com>
2026-09-24 09:51:33 -0500

    Initial commit

---
+++ src/main/java/com/marz/soporte/marz_soporte/MarzSoporteApplication.java
@@ -0,0 +1,13 @@
+package com.marz.soporte.marz_soporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarzSoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarzSoporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marz/soporte/marz_soporte/MarzSoporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marz.soporte.marz_soporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarzSoporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
